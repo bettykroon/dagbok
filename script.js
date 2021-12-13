c6827da84144b22fd26eebb483b43d616491827c
bettykroon <betty.kroon@hotmail.com>
2021-12-13 13:53:40 +0100

    dagbok

--- script.js
+++ script.js
@@ -1,20 +1,73 @@
-let myDiv = document.getElementById("myDiv");
-let myText = document.getElementById("myText");
-let myBtn = document.getElementById("myBtn");
-
-myBtn.addEventListener("click", function(){
-    let textValue = myText.value;
-    console.log(textValue);
-
-    if(textValue) {
-        localStorage.setItem("inlägg", textValue);
-        location.reload();
-    }
+window.addEventListener("load", () => {
+    renderPosts()
 })
 
-for(let i = 0; i < localStorage.length; i++) {
-    let key = localStorage.key(i);
-    let value = localStorage.getItem(key);
+let titleInput = document.getElementById("titleInput")
+let descriptionInput = document.getElementById("descriptionInput")
+let dateInput = document.getElementById("dateInput")
+let postContainer = document.getElementById("postContainer")
 
-    myDiv.innerHTML += `${key}: ${value}<br/>`
+document.getElementsByTagName("button")[0].addEventListener("click", addNewPost)
+
+function addNewPost() {
+    let newPost = {
+        title: titleInput.value,
+        description: descriptionInput.value,
+        date: dateInput.value
+    }
+
+    let collectedPosts = localStorage.getItem("posts")
+
+    if(collectedPosts) {
+        collectedPosts = JSON.parse(collectedPosts)
+    } else {
+        collectedPosts = []
+    }
+
+    collectedPosts.push(newPost)
+
+    localStorage.setItem("posts", JSON.stringify(collectedPosts))
+
+    renderPosts()
+}
+
+function renderPosts() {
+
+    postContainer.innerHTML = ""
+
+    let posts = localStorage.getItem("posts")
+
+    if(posts) {
+        posts = JSON.parse(posts)
+    } else {
+        let emptyHeader = document.createElement("h2")
+        emptyHeader.innerText = "Det fanns inga posts 😥"
+        postContainer.append(emptyHeader)
+        return
+    }
+
+    posts.sort(function(a,b){
+        // Turn your strings into dates, and then subtract them
+        // to get a value that is either negative, positive, or zero.
+        return new Date(b.date) + new Date(a.date);
+    });
+
+    posts.forEach((post) => {
+        console.log(post)
+
+        let postDiv = document.createElement("div")
+        postDiv.style.margin = "50px"
+
+        let titleText = document.createElement("h3")
+        titleText.innerText = post.title
+        
+        let descText = document.createElement("h3")
+        descText.innerText = post.description
+        
+        let dateText = document.createElement("h3")
+        dateText.innerText = post.date
+
+        postDiv.append(titleText, descText, dateText)
+        postContainer.append(postDiv)
+    })
 }
